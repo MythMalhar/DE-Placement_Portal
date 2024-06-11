a211f936bf5289278dd14f6e38bdbb54bb30c15b
Malhar <=>
2024-06-11 17:42:24 +0530

    changes of paths in index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./config/dbConfig.js";
-dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import uniformRoutes from "./routes/uniformRoutes.js";
@@ -10,32 +9,33 @@ import mailRoutes from "./routes/mailRoutes.js";
 import cors from "cors";
 import path from 'path';
 
+dotenv.config();
 db();
 
+const app = express();
 const __dirname = path.resolve();
 
-const app = express();
+// Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client', 'dist', 'index.html')));
 app.use(cors());
 
+// Static files
+app.use(express.static(path.resolve(__dirname, "dist")));
 
+// Routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/uniform", uniformRoutes);
 app.use("/api/chats", messageRoutes);
 app.use("/api/mail", mailRoutes);
 
+// Catch-all route for client-side routing
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
